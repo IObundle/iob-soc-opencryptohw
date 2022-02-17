76d37788659ad09b42d770343d1f5f4300e078be
pmiranda <pmiranda.iobundle@gmail.com>
2022-02-17 10:49:09 +0000

    feat(pc-emul): profile more function calls - Add profile tracking for more `sha256` library functions - Update report accordingly - Working for `pc-emul`

--- software/firmware/profile.h
+++ software/firmware/profile.h
@@ -10,6 +10,14 @@ extern unsigned int prof_global_us[P_NVARS];
 extern unsigned int prof_sha256_us[P_NVARS];
 extern unsigned int prof_GetHexadecimal_us[P_NVARS];
 extern unsigned int prof_printf_us[P_NVARS];
+extern unsigned int prof_sha_init_us[P_NVARS];
+extern unsigned int prof_sha_finalize_us[P_NVARS];
+extern unsigned int prof_sha_ctxrelease_us[P_NVARS];
+extern unsigned int prof_crypto_hashblocks_us[P_NVARS];
+extern unsigned int prof_ld_big_endian_us[P_NVARS];
+extern unsigned int prof_st_big_endian_us[P_NVARS];
+extern unsigned int prof_F_32_us[P_NVARS];
+extern unsigned int prof_Expand32_us[P_NVARS];
 
 #define PROF_START(var) {prof_##var##_us[P_STR] = timer_time_us();}
 #define PROF_STOP(var) {prof_##var##_us[P_END] = timer_time_us(); \
