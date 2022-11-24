1b86e87b444ef730ce02336b9e1eb96cdb239a37
PMiranda <pmiranda.iobundle@gmail.com>
2022-11-24 14:16:02 +0000

    feat(fusesoc): remove redundant scripts, add to CI - remove unnecessary scripts - rename validate.sh to validate_hook.sh for more explicit name - add fusesoc simulation to CI workflows

--- scripts/input_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Variables
-TEST_DIR="software/test"
-TEST_VECTOR_RSP=SHA256ShortMsg
-# Generate soc-in.bin
-make -C $TEST_DIR gen_test_data
-# Copy to root dir
-cp $TEST_DIR/*_d_in.bin soc-in.bin
-cp $TEST_DIR/*_d_out.bin "$TEST_VECTOR_RSP"_d_out.bin
--- scripts/versat_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Variables
-PC_DIR="software/pc-emul"
-# Build versat verilog sources
-make pc-emul-gen-versat
-# Copy versat sources to top level
-cp -u $PC_DIR/versat_instance.v .
-cp -u $PC_DIR/versat_defs.vh .
-cp -u $PC_DIR/src/* .
