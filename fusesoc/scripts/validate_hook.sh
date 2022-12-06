04bc2cd59416a8fafe35ef087bbaf24867b74e9f
PMiranda <pmiranda.iobundle@gmail.com>
2022-12-06 16:51:41 +0000

    feat(fusesoc): fpga run improvements - add missing files to .core - move console fpga hook to post run - fix console fpga and validation hook - update eth_console to run console for fusesoc build - update targets for fpga build and run - add pyserial to python requirements

--- fusesoc/scripts/validate_hook.sh
+++ fusesoc/scripts/validate_hook.sh
@@ -2,6 +2,11 @@
 # Variables
 VALIDATION_OUT_BIN=SHA256ShortMsg_d_out.bin
 SOC_OUT_BIN=soc-out.bin
+
+# SOC_OUT_BIN in sha256_test.py directory
+SOC_OUT_BIN=$(find . -name $SOC_OUT_BIN | head -n 1)
+
+# Validate test
 if python3 validate_test.py $VALIDATION_OUT_BIN $SOC_OUT_BIN
 then 
     printf "\n\nShortMessage Test PASSED\n\n"; 
