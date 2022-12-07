abf504e51d4b89807efdb23305e1d20b8873faff
PMiranda <pmiranda.iobundle@gmail.com>
2022-12-07 12:38:40 +0000

    feat(fusesoc): queue hooks, refactor makefile - add queue in and queue out hooks     - queue out functionality added to console_fpga_hook - update fusesoc makefile to run fpga from remote machine - add remote clean target

--- fusesoc/scripts/console_fpga_hook.sh
+++ fusesoc/scripts/console_fpga_hook.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
+
+BOARD=AES-KU040-DB-G
+LOAD_FILE=/tmp/$BOARD.load
+QUEUE_FILE=/tmp/$BOARD.queue
+
+FPGA_OBJ=$(ls *.bit)
+JOB=$(echo $USER `md5sum $FPGA_OBJ  | cut -d" " -f1`)
+
+# Queue out function
+queue_out () {
+	sed '/$JOB/d' $QUEUE_FILE > queue; 
+    cat queue > $QUEUE_FILE; 
+    rm queue
+}
+
+# Load file
+echo $JOB > $LOAD_FILE;
+# Run console
 python3 ./software/console/eth_console -s /dev/usb-uart -f
+# Remove from queue
+queue_out
