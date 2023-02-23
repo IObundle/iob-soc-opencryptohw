7d6b6dc139257524510c9c9f0a887327569491a2
PMiranda <pmiranda.iobundle@gmail.com>
2023-02-23 15:33:09 +0000

    feat(fusesoc): AES256 simulation fusesoc flow

--- fusesoc/scripts/validate_hook.sh
+++ fusesoc/scripts/validate_hook.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
+# Validate test results
+# Usage: ./path/to/validate_hook.sh {SHA256,AES256}
+
 # Variables
-VALIDATION_OUT_BIN=SHA256ShortMsg_d_out.bin
+if [[ "$1" == "SHA256" ]]
+then
+    VALIDATION_OUT_BIN=SHA256ShortMsg_d_out.bin
+elif [[ "$1" == "AES256" ]]
+then
+    VALIDATION_OUT_BIN=AESECB128_d_out.bin
+else
+    VALIDATION_OUT_BIN=""
+fi
+
 SOC_OUT_BIN=soc-out.bin
 
 # SOC_OUT_BIN in sha256_test.py directory
