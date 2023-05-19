8b2bd3d7de93913d197dd1d8d756b3edbc6c685d
PMiranda <pmiranda.iobundle@gmail.com>
2023-05-19 17:01:39 +0100

    feat(fusesoc): fusesoc fpga AES256 - update AES256 fusesoc fpga flow

--- fusesoc/scripts/console_fpga_hook.sh
+++ fusesoc/scripts/console_fpga_hook.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# Console FPGA Hook
+# Usage: ./path/to/console_fpga_hook.sh {SHA256,AES256,MCELIECE}
 
 # grab board again, do not program, but run console program
+if [[ "$1" == "AES256" ]]
+then
+python3 ./software/python/board_client.py grab 600 -p 'echo dummy program command' -c 'python3 ./submodules/LIB/software/python/console -s /dev/usb-uart -f'
+else
 python3 ./software/python/board_client.py grab 600 -p 'echo dummy program command' -c 'python3 ./software/console/eth_console -s /dev/usb-uart -f'
+fi
--- fusesoc/scripts/fpga-AES256-fix.tcl
+++ fusesoc/scripts/fpga-AES256-fix.tcl
@@ -78,7 +78,10 @@ read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardwar
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/MyAddressGen.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/PipelineRegister.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/Reg.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/StridedMerge.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/SwapEndian.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/TestConst.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/TimedMux.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/VRead.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/VWrite.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/src/xaddrgen2.v}
@@ -96,9 +99,14 @@ read_verilog -sv {../src/iobundle_opencryptohw_0.0.1_0/submodules/LIB/hardware/i
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-system_gen_hw_aes256_0/system.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/versat_instance.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AddRoundKey.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AES256Path.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AES256WithIterative.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AESPathExample.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AES.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/AESWithIterative.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/CH.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/Comb_F_Stage.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/Comb_M_Stage.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ComplexAdder.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ComplexMultiplier.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ComplexShareConfig.v}
@@ -106,12 +114,14 @@ read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/softwar
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ConvolutionStage.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/Convolution.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/DoRow.v}
-read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/FourthLineKey.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/FirstLineKey.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/FourthLineKey.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/F_Stage.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/F.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/Iterative_SHA_F.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/KeySchedule256.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/KeySchedule.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/MainRoundAndKey.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/MainRound.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/Maj.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/MatrixMultiplication.v}
@@ -122,6 +132,7 @@ read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/softwar
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/M.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/OnlyInputToOutput.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ReadWriteAES.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/RoundPairAndKey.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/SBox.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/SemiComplexAdder.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/ShaSingleState.v}
@@ -140,6 +151,7 @@ read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/softwar
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/T1.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/T2.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/T_Stage.v}
+read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/VectorLikeOperation.v}
 read_verilog -sv {../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul/src/VReadToVWrite.v}
 
 set_property part xcku040-fbva676-1-c [current_project]
@@ -149,7 +161,7 @@ source {../src/iobundle_opencryptohw_0.0.1_0/hardware/fpga/vivado/ddr_ip.tcl}
 
 set_property include_dirs [list ../src/iobundle_opencryptohw_0.0.1_0/submodules/CACHE/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/UART/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/LIB/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/TIMER/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/ETHERNET/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/AXI/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/submodules/VERSAT/hardware/include ../src/iobundle_opencryptohw_0.0.1_0/hardware/include ../src/iobundle_opencryptohw_0.0.1-cache_if_gen_0 ../src/iobundle_opencryptohw_0.0.1-uart_mkregs_gen_hw_0 ../src/iobundle_opencryptohw_0.0.1-timer_mkregs_gen_hw_0 ../src/iobundle_opencryptohw_0.0.1-ethernet_mkregs_gen_hw_0 ../src/iobundle_opencryptohw_0.0.1-versat_if_gen_0 ../src/iobundle_opencryptohw_0.0.1-system_gen_hw_aes256_0 ../src/iobundle_opencryptohw_0.0.1-versat_gen_aes256_0/software/pc-emul .] [get_filesets sources_1]
 
-set_property verilog_define {RUN_EXTMEM=1 USE_DDR=1 INIT_MEM=0 DATA_W=32 ADDR_W=32 BOOTROM_ADDR_W=12 SRAM_ADDR_W=12 FIRM_ADDR_W=18 DCACHE_ADDR_W=24 N_SLAVES=4 E=31 P=30 B=29 UART=0 TIMER=1 VERSAT=2 ETHERNET=3 N_SLAVES_W=2 HARDWARE_TEST=10 USE_MUL_DIV=1 USE_COMPRESSED=1 DDR_DATA_W=32 DDR_ADDR_W=30 AXI_ADDR_W=32 } [get_filesets sources_1]
+set_property verilog_define {RUN_EXTMEM=1 USE_DDR=1 INIT_MEM=0 DATA_W=32 ADDR_W=32 BOOTROM_ADDR_W=12 SRAM_ADDR_W=12 FIRM_ADDR_W=18 DCACHE_ADDR_W=24 N_SLAVES=4 E=31 P=30 B=29 UART=0 TIMER=1 VERSAT=2 ETHERNET=3 N_SLAVES_W=2 HARDWARE_TEST=1 USE_MUL_DIV=1 USE_COMPRESSED=1 DDR_DATA_W=32 DDR_ADDR_W=30 AXI_ADDR_W=32 } [get_filesets sources_1]
 
 set_property top top_system [current_fileset]
 # set_property source_mgmt_mode None [current_project]
